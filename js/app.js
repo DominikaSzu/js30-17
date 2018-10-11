a79967d7a44be76d14d31b156e4cb725592f2c6b
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-11 14:52:09 +0200

    Sorting array without articles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function getItRight (bandName) {
+    return bandName.replace(/(^a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort(function(a,b) {
+  if (getItRight(a) > getItRight(b)) {
+      return 1;
+  } else {
+      return -1;
+  }
+});
+
+console.log(sortedBands);
