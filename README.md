0e86c6f1ce89827c35ca96318eda37999a62a00a
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-15 12:36:05 +0200

    README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # js30-17
+
+This is the seventeenth of the Wes Bos Javascript30 Challenge. It is an array sort exercise with a nice photo in the background.
+
+## Code dependencies
+
+This project was completed thanks to:
+- [JavaScript30][1]
+
+[1]: https://javascript30.com/
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bandList = document.querySelector('#bands');
 
 function getItRight (bandName) {
     return bandName.replace(/(^a |an |the )/i, '').trim();
